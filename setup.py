cdba90606fbb369830711085fae9f7329d5b1907
Ate <poortinga.ate@gmail.com>
2018-04-06 11:28:32 +0700

    changed to landcover package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup
 
-setup(name='landcovertool',
+setup(name='landcoverPackage',
       version='0.1',
       description='code to create composites, training dataset and primitives',
       url='http://github.com/Servir-Mekong/landcoverPackage',
